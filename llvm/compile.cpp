055f18a214a4067d58ffc4a43a14869fad6d1b53
rlt3 <rlt3@utk.edu>
2022-06-29 12:37:08 -0400

    Simplifying the JIT which allows intra-module linking.

---
+++ llvm/compile.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h" // llvm::errs
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/Twine.h"
+#include "llvm/MC/MCContext.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/TargetRegistry.h" // TODO: handle 'llvm/MC/TargetRegistry.h' on different platforms
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/SmallVectorMemoryBuffer.h"
+
+int
+main (int argc, char **argv)
+{
+    LLVMContext context;
+    std::string errstr;
+    SMDiagnostic errhandler;
+
+    // only initialize the native target
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmParser();
+    llvm::InitializeNativeTargetAsmPrinter();
+
+    // create a MemoryBuffer from our basic IR 
+    std::string basicIR = 
+        "define i32 @main() {\n"
+        "\tret i32 72\n"
+        "}";
+    std::unique_ptr<MemoryBuffer> IRbuff = MemoryBuffer::getMemBuffer(basicIR);
+
+    // create a Module from IR parsed
+    std::unique_ptr<Module> module = parseIR(*IRbuff, errhandler, context);
+
+    // get the native target
+    auto target_triple = sys::getDefaultTargetTriple();
+    auto target = TargetRegistry::lookupTarget(target_triple, errstr);
+    if (!target) {
+        errs() << errstr << "\n";
+        return 1;
+    }
+
+    // and construct a target machine from the native target
+    auto machine_cpu = "generic";
+    auto machine_features = "";
+    TargetOptions machine_options;
+    auto machine_reloc = Optional<Reloc::Model>();
+    auto target_machine = target->createTargetMachine(target_triple,
+            machine_cpu, machine_features, machine_options, machine_reloc);
+
+    // update our model with the specific features of the native target
+    module->setTargetTriple(sys::getDefaultTargetTriple());
+    module->setDataLayout(target_machine->createDataLayout());
+
+    // create a buffer for our machine code
+    SmallVector<char, 4096> objbuffer;
+      raw_svector_ostream objstream(objbuffer);
+
+    // create a pass manager, add the passes to emit machine code, and run them
+    legacy::PassManager passes;
+    MCContext *mccontext;
+    if (target_machine->addPassesToEmitMC(passes, mccontext, objstream)) {
+        errs() << "NativeTarget doesn't support emitting machine code!\n";
+        return 1;
+    }
+    passes.run(*module);
+
+    // flushes the objstream and initializes MemoryBuffer from objbuffer
+    std::unique_ptr<MemoryBuffer> mcbuffer(
+            new SmallVectorMemoryBuffer(std::move(objbuffer)));
+
+    module->print(errs(), nullptr);
+
+    // note: this binary isn't fully linked so it won't execute
+    std::ofstream bin;
+    bin.open("a.bin", std::ios::binary);
+    bin.write(mcbuffer->getBufferStart(), mcbuffer->getBufferSize());
+    bin.close();
+
+    //std::string mangledName;
+    //raw_string_ostream mangledNameStream(mangledName);
+    //Mangler::getNameWithPrefix(mangledNameStream, "main", module->getDataLayout());
+
+    Function *call = module->getFunction("main");
+    if (!call) {
+        errs() << "Didn't find main!\n";
+    } else {
+        outs() << "Found main!!!\n";
+    }
+
+    return 0;
+}
--- llvm/main.cpp
+++ llvm/main.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
-#include <fstream>
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
+
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Verifier.h" // llvm::errs
+#include "llvm/IR/Verifier.h"
+#include "llvm/IR/DataLayout.h"
 #include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Target/TargetMachine.h"
-#include "llvm/Target/TargetOptions.h"
+#include "llvm/IR/Mangler.h"
 #include "llvm/IRReader/IRReader.h"
-#include "llvm/ADT/StringRef.h"
-#include "llvm/ADT/Twine.h"
-#include "llvm/MC/MCContext.h"
-#include "llvm/MC/MCSymbol.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/TargetRegistry.h" // TODO: handle 'llvm/MC/TargetRegistry.h' on different platforms
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/SmallVectorMemoryBuffer.h"
-
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ExecutionEngine/ExecutionEngine.h"
 #include "llvm/ExecutionEngine/JITSymbol.h"
+#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
 #include "llvm/ExecutionEngine/Orc/CompileOnDemandLayer.h"
 #include "llvm/ExecutionEngine/Orc/CompileUtils.h"
 #include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
@@ -28,250 +25,231 @@
 #include "llvm/ExecutionEngine/RTDyldMemoryManager.h"
 #include "llvm/ExecutionEngine/RuntimeDyld.h"
 #include "llvm/ExecutionEngine/SectionMemoryManager.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/Mangler.h"
 #include "llvm/Support/DynamicLibrary.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/SmallVectorMemoryBuffer.h"
 #include "llvm/Target/TargetMachine.h"
 #include "llvm/Transforms/InstCombine/InstCombine.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Scalar/GVN.h"
-#include <algorithm>
-#include <map>
-#include <memory>
-#include <set>
-#include <string>
-#include <vector>
 
 using namespace llvm;
 using namespace llvm::orc;
 
 typedef int (*FunctionEntry) (int, char*[]);
 
+//class JITMachine {
+//private:
+//  ExecutionSession ES;
+//  std::map<VModuleKey, std::shared_ptr<SymbolResolver>> Resolvers;
+//  std::unique_ptr<TargetMachine> TM;
+//  const DataLayout DL;
+//  LegacyRTDyldObjectLinkingLayer ObjectLayer;
+//  LegacyIRCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
+//
+//  using OptimizeFunction =
+//      std::function<std::unique_ptr<Module>(std::unique_ptr<Module>)>;
+//
+//  LegacyIRTransformLayer<decltype(CompileLayer), OptimizeFunction> OptimizeLayer;
+//
+//  std::unique_ptr<JITCompileCallbackManager> CompileCallbackManager;
+//  LegacyCompileOnDemandLayer<decltype(OptimizeLayer)> CODLayer;
+//
+//public:
+//  JITMachine()
+//      : TM(EngineBuilder().selectTarget()), DL(TM->createDataLayout()),
+//        ObjectLayer(AcknowledgeORCv1Deprecation, ES,
+//                    [this](VModuleKey K) {
+//                      return LegacyRTDyldObjectLinkingLayer::Resources{
+//                          std::make_shared<SectionMemoryManager>(),
+//                          Resolvers[K]};
+//                    }),
+//        CompileLayer(AcknowledgeORCv1Deprecation, ObjectLayer,
+//                     SimpleCompiler(*TM)),
+//        OptimizeLayer(AcknowledgeORCv1Deprecation, CompileLayer,
+//                      [this](std::unique_ptr<Module> M) {
+//                        return optimizeModule(std::move(M));
+//                      }),
+//        CompileCallbackManager(cantFail(orc::createLocalCompileCallbackManager(
+//            TM->getTargetTriple(), ES, 0))),
+//        CODLayer(
+//            AcknowledgeORCv1Deprecation, ES, OptimizeLayer,
+//            [&](orc::VModuleKey K) { return Resolvers[K]; },
+//            [&](orc::VModuleKey K, std::shared_ptr<SymbolResolver> R) {
+//              Resolvers[K] = std::move(R);
+//            },
+//            [](Function &F) { return std::set<Function *>({&F}); },
+//            *CompileCallbackManager,
+//            orc::createLocalIndirectStubsManagerBuilder(
+//                TM->getTargetTriple())) {
+//    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+//  }
+//
+//  TargetMachine &getTargetMachine() { return *TM; }
+//
+//  VModuleKey addModule(std::unique_ptr<Module> M) {
+//    // Create a new VModuleKey.
+//    VModuleKey K = ES.allocateVModule();
+//
+//    // Build a resolver and associate it with the new key.
+//    Resolvers[K] = createLegacyLookupResolver(
+//        ES,
+//        [this](const std::string &Name) -> JITSymbol {
+//          if (auto Sym = CompileLayer.findSymbol(Name, false))
+//            return Sym;
+//          else if (auto Err = Sym.takeError())
+//            return std::move(Err);
+//          if (auto SymAddr =
+//                  RTDyldMemoryManager::getSymbolAddressInProcess(Name))
+//            return JITSymbol(SymAddr, JITSymbolFlags::Exported);
+//          return nullptr;
+//        },
+//        [](Error Err) { cantFail(std::move(Err), "lookupFlags failed"); });
+//
+//    // Add the module to the JIT with the new key.
+//    cantFail(CODLayer.addModule(K, std::move(M)));
+//    return K;
+//  }
+//
+//  JITSymbol findSymbol(const std::string Name) {
+//    std::string MangledName;
+//    raw_string_ostream MangledNameStream(MangledName);
+//    Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
+//    return CODLayer.findSymbol(MangledNameStream.str(), true);
+//  }
+//
+//  void removeModule(VModuleKey K) {
+//    cantFail(CODLayer.removeModule(K));
+//  }
+//
+//private:
+//  std::unique_ptr<Module> optimizeModule(std::unique_ptr<Module> M) {
+//    // Create a function pass manager.
+//    auto FPM = std::make_unique<legacy::FunctionPassManager>(M.get());
+//
+//    // Add some optimizations.
+//    FPM->add(createInstructionCombiningPass());
+//    FPM->add(createReassociatePass());
+//    FPM->add(createGVNPass());
+//    FPM->add(createCFGSimplificationPass());
+//    FPM->doInitialization();
+//
+//    // Run the optimizations over all functions in the module being added to
+//    // the JIT.
+//    for (auto &F : *M)
+//      FPM->run(F);
+//
+//    return M;
+//  }
+//};
+
 class JITMachine {
 private:
   ExecutionSession ES;
-  std::map<VModuleKey, std::shared_ptr<SymbolResolver>> Resolvers;
-  std::unique_ptr<TargetMachine> TM;
-  const DataLayout DL;
-  LegacyRTDyldObjectLinkingLayer ObjectLayer;
-  LegacyIRCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
+  RTDyldObjectLinkingLayer ObjectLayer;
+  IRCompileLayer CompileLayer;
 
-  using OptimizeFunction =
-      std::function<std::unique_ptr<Module>(std::unique_ptr<Module>)>;
+  DataLayout DL;
+  MangleAndInterner Mangle;
+  ThreadSafeContext Ctx;
 
-  LegacyIRTransformLayer<decltype(CompileLayer), OptimizeFunction> OptimizeLayer;
-
-  std::unique_ptr<JITCompileCallbackManager> CompileCallbackManager;
-  LegacyCompileOnDemandLayer<decltype(OptimizeLayer)> CODLayer;
+  JITDylib &MainJD;
 
 public:
-  JITMachine()
-      : TM(EngineBuilder().selectTarget()), DL(TM->createDataLayout()),
-        ObjectLayer(AcknowledgeORCv1Deprecation, ES,
-                    [this](VModuleKey K) {
-                      return LegacyRTDyldObjectLinkingLayer::Resources{
-                          std::make_shared<SectionMemoryManager>(),
-                          Resolvers[K]};
-                    }),
-        CompileLayer(AcknowledgeORCv1Deprecation, ObjectLayer,
-                     SimpleCompiler(*TM)),
-        OptimizeLayer(AcknowledgeORCv1Deprecation, CompileLayer,
-                      [this](std::unique_ptr<Module> M) {
-                        return optimizeModule(std::move(M));
-                      }),
-        CompileCallbackManager(cantFail(orc::createLocalCompileCallbackManager(
-            TM->getTargetTriple(), ES, 0))),
-        CODLayer(
-            AcknowledgeORCv1Deprecation, ES, OptimizeLayer,
-            [&](orc::VModuleKey K) { return Resolvers[K]; },
-            [&](orc::VModuleKey K, std::shared_ptr<SymbolResolver> R) {
-              Resolvers[K] = std::move(R);
-            },
-            [](Function &F) { return std::set<Function *>({&F}); },
-            *CompileCallbackManager,
-            orc::createLocalIndirectStubsManagerBuilder(
-                TM->getTargetTriple())) {
-    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+  JITMachine(JITTargetMachineBuilder JTMB, DataLayout DL)
+      : ObjectLayer(ES,
+                    []() { return std::make_unique<SectionMemoryManager>(); }),
+        CompileLayer(ES, ObjectLayer,
+                     std::make_unique<ConcurrentIRCompiler>(std::move(JTMB))),
+        DL(std::move(DL)), Mangle(ES, this->DL),
+        Ctx(std::make_unique<LLVMContext>()),
+        MainJD(ES.createJITDylib("<main>")) {
+    MainJD.addGenerator(
+        cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(
+            DL.getGlobalPrefix())));
   }
 
-  TargetMachine &getTargetMachine() { return *TM; }
+  static Expected<std::unique_ptr<JITMachine>> Create() {
+    auto JTMB = JITTargetMachineBuilder::detectHost();
 
-  VModuleKey addModule(std::unique_ptr<Module> M) {
-    // Create a new VModuleKey.
-    VModuleKey K = ES.allocateVModule();
+    if (!JTMB)
+      return JTMB.takeError();
 
-    // Build a resolver and associate it with the new key.
-    Resolvers[K] = createLegacyLookupResolver(
-        ES,
-        [this](const std::string &Name) -> JITSymbol {
-          if (auto Sym = CompileLayer.findSymbol(Name, false))
-            return Sym;
-          else if (auto Err = Sym.takeError())
-            return std::move(Err);
-          if (auto SymAddr =
-                  RTDyldMemoryManager::getSymbolAddressInProcess(Name))
-            return JITSymbol(SymAddr, JITSymbolFlags::Exported);
-          return nullptr;
-        },
-        [](Error Err) { cantFail(std::move(Err), "lookupFlags failed"); });
+    auto DL = JTMB->getDefaultDataLayoutForTarget();
+    if (!DL)
+      return DL.takeError();
 
-    // Add the module to the JIT with the new key.
-    cantFail(CODLayer.addModule(K, std::move(M)));
-    return K;
+    return std::make_unique<JITMachine>(std::move(*JTMB), std::move(*DL));
   }
 
-  JITSymbol findSymbol(const std::string Name) {
-    std::string MangledName;
-    raw_string_ostream MangledNameStream(MangledName);
-    Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
-    return CODLayer.findSymbol(MangledNameStream.str(), true);
+  const DataLayout &getDataLayout() const { return DL; }
+
+  LLVMContext &getContext() { return *Ctx.getContext(); }
+
+  Error addModule(std::unique_ptr<Module> M) {
+    return CompileLayer.add(MainJD, ThreadSafeModule(std::move(M), Ctx));
   }
 
-  void removeModule(VModuleKey K) {
-    cantFail(CODLayer.removeModule(K));
-  }
-
-private:
-  std::unique_ptr<Module> optimizeModule(std::unique_ptr<Module> M) {
-    // Create a function pass manager.
-    auto FPM = std::make_unique<legacy::FunctionPassManager>(M.get());
-
-    // Add some optimizations.
-    FPM->add(createInstructionCombiningPass());
-    FPM->add(createReassociatePass());
-    FPM->add(createGVNPass());
-    FPM->add(createCFGSimplificationPass());
-    FPM->doInitialization();
-
-    // Run the optimizations over all functions in the module being added to
-    // the JIT.
-    for (auto &F : *M)
-      FPM->run(F);
-
-    return M;
+  Expected<JITEvaluatedSymbol> lookup(StringRef Name) {
+    return ES.lookup({&MainJD}, Mangle(Name.str()));
   }
 };
+
+std::unique_ptr<Module>
+compileIR (LLVMContext &ctx, std::string IR)
+{
+    SMDiagnostic errhandler;
+    std::unique_ptr<MemoryBuffer> IRbuff = MemoryBuffer::getMemBuffer(IR);
+    auto m = parseIR(*IRbuff, errhandler, ctx);
+    if (!m) {
+        errhandler.print("JIT", errs());
+        exit(1);
+    }
+    return m;
+}
 
 int
 main (int argc, char **argv)
 {
     LLVMContext context;
-    std::string errstr;
-    SMDiagnostic errhandler;
 
     // only initialize the native target
     llvm::InitializeNativeTarget();
     llvm::InitializeNativeTargetAsmParser();
     llvm::InitializeNativeTargetAsmPrinter();
 
-    // create a MemoryBuffer from our basic IR 
-    std::string basicIR = 
+    // Create two seperate IR modules which reference one another but are
+    // compiled separately
+    std::string mainIR = 
+        "declare i32 @foo()\n"
         "define i32 @main() {\n"
-        "\tret i32 72\n"
+        "\t%result = call i32 @foo()\n"
+        "\tret i32 %result\n"
         "}";
-    std::unique_ptr<MemoryBuffer> IRbuff = MemoryBuffer::getMemBuffer(basicIR);
+    std::string fooIR = 
+        "define i32 @foo() {\n"
+        "\tret i32 33\n"
+        "}";
+    //std::string redefineIR = 
+    //    "define i32 @foo() {\n"
+    //    "\tret i32 72\n"
+    //    "}";
+    auto main_module = compileIR(context, mainIR);
+    auto foo_module = compileIR(context, fooIR);
 
-    // create a Module from IR parsed
-    std::unique_ptr<Module> ir_module = parseIR(*IRbuff, errhandler, context);
+    std::unique_ptr<JITMachine> JIT = cantFail(JITMachine::Create());
+    auto err1 = JIT->addModule(std::move(main_module));
+    auto err2 = JIT->addModule(std::move(foo_module));
 
-    // add the compiled ir to the JIT, compiling it to machine code
-    JITMachine JIT;
-    JIT.addModule(std::move(ir_module));
-
-    // i think the following compiles the JIT module somehow?
-    std::unique_ptr<Module> module = std::make_unique<Module>("my cool jit", context);
-    module->setDataLayout(JIT.getTargetMachine().createDataLayout());
-
-    auto main = JIT.findSymbol("main");
+    auto main = JIT->lookup("main");
     if (!main) {
-        errs() << "Couldn't find main!\n";
+        errs() << "Couldn't find symbol!\n";
         return 1;
     }
-    FunctionEntry entry = (FunctionEntry) (intptr_t) cantFail(main.getAddress());
+    FunctionEntry entry = (FunctionEntry) (intptr_t) main->getAddress();
     outs() << "main returns: " << entry(0, NULL) << "\n";
 
     return 0;
 }
-
-//int
-//main (int argc, char **argv)
-//{
-//    LLVMContext context;
-//    std::string errstr;
-//    SMDiagnostic errhandler;
-//
-//    // only initialize the native target
-//    llvm::InitializeNativeTarget();
-//    llvm::InitializeNativeTargetAsmParser();
-//    llvm::InitializeNativeTargetAsmPrinter();
-//
-//    // create a MemoryBuffer from our basic IR 
-//    std::string basicIR = 
-//        "define i32 @main() {\n"
-//        "\tret i32 72\n"
-//        "}";
-//    std::unique_ptr<MemoryBuffer> IRbuff = MemoryBuffer::getMemBuffer(basicIR);
-//
-//    // create a Module from IR parsed
-//    std::unique_ptr<Module> module = parseIR(*IRbuff, errhandler, context);
-//
-//    // get the native target
-//    auto target_triple = sys::getDefaultTargetTriple();
-//    auto target = TargetRegistry::lookupTarget(target_triple, errstr);
-//    if (!target) {
-//        errs() << errstr << "\n";
-//        return 1;
-//    }
-//
-//    // and construct a target machine from the native target
-//    auto machine_cpu = "generic";
-//    auto machine_features = "";
-//    TargetOptions machine_options;
-//    auto machine_reloc = Optional<Reloc::Model>();
-//    auto target_machine = target->createTargetMachine(target_triple,
-//            machine_cpu, machine_features, machine_options, machine_reloc);
-//
-//    // update our model with the specific features of the native target
-//    module->setTargetTriple(sys::getDefaultTargetTriple());
-//    module->setDataLayout(target_machine->createDataLayout());
-//
-//    // create a buffer for our machine code
-//    SmallVector<char, 4096> objbuffer;
-//      raw_svector_ostream objstream(objbuffer);
-//
-//    // create a pass manager, add the passes to emit machine code, and run them
-//    legacy::PassManager passes;
-//    MCContext *mccontext;
-//    if (target_machine->addPassesToEmitMC(passes, mccontext, objstream)) {
-//        errs() << "NativeTarget doesn't support emitting machine code!\n";
-//        return 1;
-//    }
-//    passes.run(*module);
-//
-//    // flushes the objstream and initializes MemoryBuffer from objbuffer
-//    std::unique_ptr<MemoryBuffer> mcbuffer(
-//            new SmallVectorMemoryBuffer(std::move(objbuffer)));
-//
-//    module->print(errs(), nullptr);
-//
-//    // note: this binary isn't fully linked so it won't execute
-//    std::ofstream bin;
-//    bin.open("a.bin", std::ios::binary);
-//    bin.write(mcbuffer->getBufferStart(), mcbuffer->getBufferSize());
-//    bin.close();
-//
-//    //std::string mangledName;
-//    //raw_string_ostream mangledNameStream(mangledName);
-//    //Mangler::getNameWithPrefix(mangledNameStream, "main", module->getDataLayout());
-//
-//    Function *call = module->getFunction("main");
-//    if (!call) {
-//        errs() << "Didn't find main!\n";
-//    } else {
-//        outs() << "Found main!!!\n";
-//    }
-//
-//    return 0;
-//}
