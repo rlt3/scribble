c345664f18d3fa49d26e76e25d616c091070ddaf
rlt3 <rlt3@utk.edu>
2022-06-30 17:24:25 -0400

    Updated formatting of JITMachine (to be readable). Added small demonstration on how individual modules would need to be mapped.

--- llvm/main.cpp
+++ llvm/main.cpp
@@ -40,161 +40,118 @@ using namespace llvm::orc;
 
 typedef int (*FunctionEntry) (int, char*[]);
 
-//class JITMachine {
-//private:
-//  ExecutionSession ES;
-//  std::map<VModuleKey, std::shared_ptr<SymbolResolver>> Resolvers;
-//  std::unique_ptr<TargetMachine> TM;
-//  const DataLayout DL;
-//  LegacyRTDyldObjectLinkingLayer ObjectLayer;
-//  LegacyIRCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
-//
-//  using OptimizeFunction =
-//      std::function<std::unique_ptr<Module>(std::unique_ptr<Module>)>;
-//
-//  LegacyIRTransformLayer<decltype(CompileLayer), OptimizeFunction> OptimizeLayer;
-//
-//  std::unique_ptr<JITCompileCallbackManager> CompileCallbackManager;
-//  LegacyCompileOnDemandLayer<decltype(OptimizeLayer)> CODLayer;
-//
-//public:
-//  JITMachine()
-//      : TM(EngineBuilder().selectTarget()), DL(TM->createDataLayout()),
-//        ObjectLayer(AcknowledgeORCv1Deprecation, ES,
-//                    [this](VModuleKey K) {
-//                      return LegacyRTDyldObjectLinkingLayer::Resources{
-//                          std::make_shared<SectionMemoryManager>(),
-//                          Resolvers[K]};
-//                    }),
-//        CompileLayer(AcknowledgeORCv1Deprecation, ObjectLayer,
-//                     SimpleCompiler(*TM)),
-//        OptimizeLayer(AcknowledgeORCv1Deprecation, CompileLayer,
-//                      [this](std::unique_ptr<Module> M) {
-//                        return optimizeModule(std::move(M));
-//                      }),
-//        CompileCallbackManager(cantFail(orc::createLocalCompileCallbackManager(
-//            TM->getTargetTriple(), ES, 0))),
-//        CODLayer(
-//            AcknowledgeORCv1Deprecation, ES, OptimizeLayer,
-//            [&](orc::VModuleKey K) { return Resolvers[K]; },
-//            [&](orc::VModuleKey K, std::shared_ptr<SymbolResolver> R) {
-//              Resolvers[K] = std::move(R);
-//            },
-//            [](Function &F) { return std::set<Function *>({&F}); },
-//            *CompileCallbackManager,
-//            orc::createLocalIndirectStubsManagerBuilder(
-//                TM->getTargetTriple())) {
-//    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
-//  }
-//
-//  TargetMachine &getTargetMachine() { return *TM; }
-//
-//  VModuleKey addModule(std::unique_ptr<Module> M) {
-//    // Create a new VModuleKey.
-//    VModuleKey K = ES.allocateVModule();
-//
-//    // Build a resolver and associate it with the new key.
-//    Resolvers[K] = createLegacyLookupResolver(
-//        ES,
-//        [this](const std::string &Name) -> JITSymbol {
-//          if (auto Sym = CompileLayer.findSymbol(Name, false))
-//            return Sym;
-//          else if (auto Err = Sym.takeError())
-//            return std::move(Err);
-//          if (auto SymAddr =
-//                  RTDyldMemoryManager::getSymbolAddressInProcess(Name))
-//            return JITSymbol(SymAddr, JITSymbolFlags::Exported);
-//          return nullptr;
-//        },
-//        [](Error Err) { cantFail(std::move(Err), "lookupFlags failed"); });
-//
-//    // Add the module to the JIT with the new key.
-//    cantFail(CODLayer.addModule(K, std::move(M)));
-//    return K;
-//  }
-//
-//  JITSymbol findSymbol(const std::string Name) {
-//    std::string MangledName;
-//    raw_string_ostream MangledNameStream(MangledName);
-//    Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
-//    return CODLayer.findSymbol(MangledNameStream.str(), true);
-//  }
-//
-//  void removeModule(VModuleKey K) {
-//    cantFail(CODLayer.removeModule(K));
-//  }
-//
-//private:
-//  std::unique_ptr<Module> optimizeModule(std::unique_ptr<Module> M) {
-//    // Create a function pass manager.
-//    auto FPM = std::make_unique<legacy::FunctionPassManager>(M.get());
-//
-//    // Add some optimizations.
-//    FPM->add(createInstructionCombiningPass());
-//    FPM->add(createReassociatePass());
-//    FPM->add(createGVNPass());
-//    FPM->add(createCFGSimplificationPass());
-//    FPM->doInitialization();
-//
-//    // Run the optimizations over all functions in the module being added to
-//    // the JIT.
-//    for (auto &F : *M)
-//      FPM->run(F);
-//
-//    return M;
-//  }
-//};
-
 class JITMachine {
 private:
-  ExecutionSession ES;
-  RTDyldObjectLinkingLayer ObjectLayer;
-  IRCompileLayer CompileLayer;
+    ExecutionSession ES;
+    std::shared_ptr<SymbolResolver> Resolver;
+    std::unique_ptr<TargetMachine> TM;
+    const DataLayout DL;
+    LegacyRTDyldObjectLinkingLayer ObjectLayer;
+    LegacyIRCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
 
-  DataLayout DL;
-  MangleAndInterner Mangle;
-  ThreadSafeContext Ctx;
+    using OptimizeFunction =
+      std::function<std::unique_ptr<Module>(std::unique_ptr<Module>)>;
 
-  JITDylib &MainJD;
+    LegacyIRTransformLayer<decltype(CompileLayer), OptimizeFunction> OptimizeLayer;
+
+    std::unique_ptr<JITCompileCallbackManager> CompileCallbackMgr;
+    std::unique_ptr<IndirectStubsManager> IndirectStubsMgr;
 
 public:
-  JITMachine(JITTargetMachineBuilder JTMB, DataLayout DL)
-      : ObjectLayer(ES,
-                    []() { return std::make_unique<SectionMemoryManager>(); }),
-        CompileLayer(ES, ObjectLayer,
-                     std::make_unique<ConcurrentIRCompiler>(std::move(JTMB))),
-        DL(std::move(DL)), Mangle(ES, this->DL),
-        Ctx(std::make_unique<LLVMContext>()),
-        MainJD(ES.createJITDylib("<main>")) {
-    MainJD.addGenerator(
-        cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(
-            DL.getGlobalPrefix())));
-  }
+    JITMachine()
+        : Resolver(createLegacyLookupResolver(ES,
+            [this](const std::string &Name) -> JITSymbol {
+                if (auto Sym = IndirectStubsMgr->findStub(Name, false))
+                    return Sym;
+                if (auto Sym = OptimizeLayer.findSymbol(Name, false))
+                    return Sym;
+                else if (auto Err = Sym.takeError())
+                    return std::move(Err);
+                if (auto SymAddr =
+                        RTDyldMemoryManager::getSymbolAddressInProcess(Name))
+                    return JITSymbol(SymAddr, JITSymbolFlags::Exported);
+                return nullptr;
+            },
+            [](Error Err) { cantFail(std::move(Err), "lookupFlags failed"); }))
+        , TM(EngineBuilder().selectTarget())
+        , DL(TM->createDataLayout())
+        , ObjectLayer(AcknowledgeORCv1Deprecation, ES,
+              [this](VModuleKey K) {
+                  return LegacyRTDyldObjectLinkingLayer::Resources{
+                    std::make_shared<SectionMemoryManager>(), Resolver};
+              })
+        , CompileLayer(AcknowledgeORCv1Deprecation,
+                ObjectLayer, SimpleCompiler(*TM))
+        , OptimizeLayer(AcknowledgeORCv1Deprecation, CompileLayer,
+              [this](std::unique_ptr<Module> M) {
+                  return optimizeModule(std::move(M));
+              })
+        , CompileCallbackMgr(cantFail(
+              orc::createLocalCompileCallbackManager(TM->getTargetTriple(), ES, 0)))
+    {
+        auto IndirectStubsMgrBuilder =
+            orc::createLocalIndirectStubsManagerBuilder(TM->getTargetTriple());
+        IndirectStubsMgr = IndirectStubsMgrBuilder();
+        llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+    }
 
-  static Expected<std::unique_ptr<JITMachine>> Create() {
-    auto JTMB = JITTargetMachineBuilder::detectHost();
+    TargetMachine&
+    getTargetMachine()
+    {
+        return *TM;
+    }
 
-    if (!JTMB)
-      return JTMB.takeError();
+    VModuleKey
+    addModule (std::unique_ptr<Module> M)
+    {
+        // Add the module to the JIT with a new VModuleKey.
+        auto K = ES.allocateVModule();
+        cantFail(OptimizeLayer.addModule(K, std::move(M)));
+        return K;
+    }
 
-    auto DL = JTMB->getDefaultDataLayoutForTarget();
-    if (!DL)
-      return DL.takeError();
+    JITSymbol
+    findSymbol(const std::string Name)
+    {
+        return OptimizeLayer.findSymbol(mangle(Name), true);
+    }
 
-    return std::make_unique<JITMachine>(std::move(*JTMB), std::move(*DL));
-  }
+    void
+    removeModule (VModuleKey K)
+    {
+        cantFail(OptimizeLayer.removeModule(K));
+    }
 
-  const DataLayout &getDataLayout() const { return DL; }
+private:
+    std::string
+    mangle (const std::string &Name)
+    {
+        std::string MangledName;
+        raw_string_ostream MangledNameStream(MangledName);
+        Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
+        return MangledNameStream.str();
+    }
 
-  LLVMContext &getContext() { return *Ctx.getContext(); }
+    std::unique_ptr<Module>
+    optimizeModule (std::unique_ptr<Module> M)
+    {
+        // Create a function pass manager.
+        auto FPM = std::make_unique<legacy::FunctionPassManager>(M.get());
 
-  Error addModule(std::unique_ptr<Module> M) {
-    return CompileLayer.add(MainJD, ThreadSafeModule(std::move(M), Ctx));
-  }
+        // Add some optimizations.
+        FPM->add(createInstructionCombiningPass());
+        FPM->add(createReassociatePass());
+        FPM->add(createGVNPass());
+        FPM->add(createCFGSimplificationPass());
+        FPM->doInitialization();
 
-  Expected<JITEvaluatedSymbol> lookup(StringRef Name) {
-    return ES.lookup({&MainJD}, Mangle(Name.str()));
-  }
+        // Run the optimizations over all functions in the module being added
+        // to the JIT.
+        for (auto &F : *M)
+            FPM->run(F);
+
+        return M;
+    }
 };
 
 std::unique_ptr<Module>
@@ -210,18 +167,29 @@ compileIR (LLVMContext &ctx, std::string IR)
     return m;
 }
 
+void
+runMain (JITMachine &JIT)
+{
+    auto main = JIT.findSymbol("main");
+    if (!main) {
+        errs() << "Couldn't find symbol!\n";
+        return;
+    }
+    FunctionEntry entry = (FunctionEntry) (intptr_t) cantFail(main.getAddress());
+    outs() << "main returns: " << entry(0, NULL) << "\n";
+}
+
 int
 main (int argc, char **argv)
 {
     LLVMContext context;
 
-    // only initialize the native target
+    /* JITMachine must be instantiated after these initializations */
     llvm::InitializeNativeTarget();
     llvm::InitializeNativeTargetAsmParser();
     llvm::InitializeNativeTargetAsmPrinter();
+    JITMachine JIT;
 
-    // Create two seperate IR modules which reference one another but are
-    // compiled separately
     std::string mainIR = 
         "declare i32 @foo()\n"
         "define i32 @main() {\n"
@@ -232,24 +200,37 @@ main (int argc, char **argv)
         "define i32 @foo() {\n"
         "\tret i32 33\n"
         "}";
-    //std::string redefineIR = 
-    //    "define i32 @foo() {\n"
-    //    "\tret i32 72\n"
-    //    "}";
-    auto main_module = compileIR(context, mainIR);
-    auto foo_module = compileIR(context, fooIR);
+    std::string redefineIR =
+        "define i32 @foo() {\n"
+        "\tret i32 72\n"
+        "}";
 
-    std::unique_ptr<JITMachine> JIT = cantFail(JITMachine::Create());
-    auto err1 = JIT->addModule(std::move(main_module));
-    auto err2 = JIT->addModule(std::move(foo_module));
+    /*
+     * We specify each module here because we can only use them once. These are
+     * unique_ptrs and thus we give ownership of them to the JIT when we add
+     * them.
+     */
+    auto main1_module = compileIR(context, mainIR);
+    auto main2_module = compileIR(context, mainIR);
+    auto foo1_module = compileIR(context, fooIR);
+    auto foo2_module = compileIR(context, redefineIR);
 
-    auto main = JIT->lookup("main");
-    if (!main) {
-        errs() << "Couldn't find symbol!\n";
-        return 1;
-    }
-    FunctionEntry entry = (FunctionEntry) (intptr_t) main->getAddress();
-    outs() << "main returns: " << entry(0, NULL) << "\n";
+    /* Define the original, return 33 */
+    auto k1 = JIT.addModule(std::move(main1_module));
+    auto k2 = JIT.addModule(std::move(foo1_module));
+    runMain(JIT);
+
+    /*
+     * `foo` has changed, so update it, attempting to runMain here would cause
+     * a segmentation fault.
+     */
+    JIT.removeModule(k2);
+    auto k3 = JIT.addModule(std::move(foo2_module));
+
+    /* now update the tree that points to `foo` */
+    JIT.removeModule(k1);
+    auto k4 = JIT.addModule(std::move(main2_module));
+    runMain(JIT);
 
     return 0;
 }
